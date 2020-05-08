66663483f83222d7423d990300c3d35fb35f88d4
Dimitriis Zeginis <zeginis@gmail.com>
2020-05-08 22:37:21 +0300

    change location of config properties class

--- backend/src/main/java/com/cybele/semanticenrichment/configuration/VirtuosoProperties.java
+++ backend/src/main/java/com/cybele/semanticenrichment/configuration/VirtuosoProperties.java
@@ -1,4 +1,4 @@
-package com.cybele.semanticenrichment.config;
+package com.cybele.semanticenrichment.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
