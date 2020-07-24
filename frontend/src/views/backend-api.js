f0b3ec80f8625801282c99c6333012d09a798fae
Dimitris Zeginis <zeginis@gmail.com>
2020-07-24 12:47:01 +0300

    Send structural metadata from frontend

--- frontend/src/views/backend-api.js
+++ frontend/src/views/backend-api.js
@@ -12,7 +12,7 @@ export default {
     createDatasetMetadata(label_value,lang_value,issued_value,modified_value,periodicity_value, 
     		temporalStart_value, temporalEnd_value, temporalResolution_value, spatial_value, 
     		spatialResolution_value,conformsTo_value,landingPage_value,publisher_value,
-    		license_value, mediaType_value, byteSize_value) {
+    		license_value, mediaType_value, byteSize_value, tableName_value,components_value) {
     	return AXIOS.post(`/dataset`,{label:label_value, 
     		                          language:lang_value,
     		                          issued:issued_value,
@@ -28,7 +28,9 @@ export default {
     		                          publisher:publisher_value,
     		                          license:license_value,
     		                          mediaType:mediaType_value,
-    		                          byteSize:byteSize_value});
+    		                          byteSize:byteSize_value,
+    		                          tableName:tableName_value,
+    		                          components:components_value});
     },
     getCodelistContent(codelistId){
     	return AXIOS.get(`/codelist/` + codelistId);    	
